8a83b72cba017a020cc638be80c5847504bb62e1
Hewlette Dibble <hcodydibble@gmail.com>
2017-10-17 15:47:41 -0700

    added lucas function and test for lucas function

--- series.py
+++ series.py
@@ -7,11 +7,15 @@ def fibonacci(n):
     if n < 0:
         raise ValueError("Please input positive integers only.")
     for i in range(n - 1):
-        fib_sequence.append(fib_sequence[i + 0] + fib_sequence[i + 1])
+        fib_sequence.append(fib_sequence[i] + fib_sequence[i + 1])
     return fib_sequence[n]
 
 
 def lucas(n):
     """Return the nth item of the lucas sequence."""
     lucas_sequence = [2, 1]
+    if n < 0:
+        raise ValueError("Please input positive integers only.")
+    for i in range(n - 1):
+        lucas_sequence.append(lucas_sequence[i] + lucas_sequence[i + 1])
     return lucas_sequence[n]
